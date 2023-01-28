ef1813f502d882fcc1b67bd67c03e1f4ae00974a
rpsingh6634 <rpsingh6634@gmail.com>
2023-01-29 00:05:34 +0530

    firstt commit

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       
       <div className="footer">
          <Typography.Title level={5} style={{color:'white', textAlign:'center'}}>
-           Cryptoverse <br/>
+           Cryptowatch <br/>
            All rights reserved
          </Typography.Title>
          <Space>
